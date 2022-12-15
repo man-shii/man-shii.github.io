cc923d2b8d6ce14fab20546101aae27827027436
manshii <71992689+man-shii@users.noreply.github.com>
2022-12-15 19:31:20 +0800

    Add files via upload

---
+++ precache-manifest.5587f741864fba3448030c30874727c5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1eee452bdcaaebeddad5dccf766d4bcb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "489a4a844ca1f14fa167",
+    "url": "/static/css/main.5d2f3e34.chunk.css"
+  },
+  {
+    "revision": "3827ae87bda2701a32dc",
+    "url": "/static/js/2.e78353bb.chunk.js"
+  },
+  {
+    "revision": "e8360af5817953cec407b3cdee6b2ff8",
+    "url": "/static/js/2.e78353bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "489a4a844ca1f14fa167",
+    "url": "/static/js/main.2960def5.chunk.js"
+  },
+  {
+    "revision": "949eb168f1d833ae83d5",
+    "url": "/static/js/runtime-main.743d20cc.js"
+  }
+]);
